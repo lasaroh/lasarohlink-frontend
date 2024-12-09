b39316c7bfd4b6801e9040eb6dda01ae0077c311
lasaroh <lazarodecouso@gmail.com>
2024-12-09 22:34:20 +0100

    - Refactor and enhance form and copy button behaviour - Update package dependencies for tailwind and astro to the latest versions

--- src/scripts/UrlShortener.js
+++ src/scripts/UrlShortener.js
@@ -1,13 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   const form = document.getElementById("url");
   const shortenedUrl = document.getElementById("shortenedUrl");
-  const divUrlShortened = document.getElementById("divUrlShortened")
+  const divUrlShortened = document.getElementById("divUrlShortened");
+  const copyButton = document.getElementById("copyButton");
+  const tooltip = document.getElementById("tooltip");
 
+  // Add a listener to the form's "submit" event
   form.addEventListener("submit", async (e) => {
     e.preventDefault();
-    
-    divUrlShortened.classList.remove('invisible');
-    
+
+    divUrlShortened.classList.remove("invisible");
     const data = new FormData(e.target);
 
     try {
@@ -17,45 +19,34 @@ document.addEventListener("DOMContentLoaded", () => {
       });
 
       const result = await response.text();
-
-      if (response.ok) {
-        shortenedUrl.value = result;
-      } else {
-        shortenedUrl.value = result;
-      }
+      shortenedUrl.value = result;
     } catch (error) {
       shortenedUrl.value = error;
     }
   });
-});
 
-const copyButton = document.getElementById("copyButton");
-copyButton.addEventListener("click", () => {
-  const shortenedUrl = document.getElementById("shortenedUrl");
-  const tooltip = document.getElementById("tooltip");
-  const buttonRect = copyButton.getBoundingClientRect();
+  // Add a "click" event listener to the copy button
+  copyButton.addEventListener("click", () => {
+    const buttonRect = copyButton.getBoundingClientRect();
 
-  // Copiar texto al portapapeles
-  navigator.clipboard.writeText(shortenedUrl.value).then(() => {
-    // Posicionar el tooltip 10px debajo del botón
-    tooltip.style.position = "absolute";
-    tooltip.style.top = `${
-      buttonRect.top + window.scrollY + buttonRect.height + 10
-    }px`; // 10px por debajo del botón
-    tooltip.style.left = `${
-      buttonRect.left +
-      window.scrollX +
-      (buttonRect.width - tooltip.offsetWidth) / 2
-    }px`; // Centrado horizontalmente
+    navigator.clipboard.writeText(shortenedUrl.value).then(() => {
+      tooltip.style.position = "absolute";
+      tooltip.style.top = `${
+        buttonRect.top + window.scrollY + buttonRect.height + 10
+      }px`;
+      tooltip.style.left = `${
+        buttonRect.left +
+        window.scrollX +
+        (buttonRect.width - tooltip.offsetWidth) / 2
+      }px`;
 
-    // Mostrar el tooltip
-    tooltip.classList.remove("opacity-0");
-    tooltip.classList.add("opacity-100");
+      tooltip.classList.remove("opacity-0");
+      tooltip.classList.add("opacity-100");
 
-    // Ocultar después de 1.5 segundos
-    setTimeout(() => {
-      tooltip.classList.remove("opacity-100");
-      tooltip.classList.add("opacity-0");
-    }, 1500);
+      setTimeout(() => {
+        tooltip.classList.remove("opacity-100");
+        tooltip.classList.add("opacity-0");
+      }, 1500);
+    });
   });
 });
